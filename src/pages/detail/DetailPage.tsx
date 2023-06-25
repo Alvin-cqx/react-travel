5da307f103739c783025c60652251261c8220298
chenqingxian <9549587@qq.com>
2023-06-25 18:34:31 +0800

    ss

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./DetailPage.module.css";
-import { RouteComponentProps } from "react-router-dom";
-interface routeInter{
-  detailId:string
+import { RouteComponentProps, useParams } from "react-router-dom";
+import axios from "axios";
+interface MatchParams {
+  detailId: string;
 }
-export const DetailPage:React.FC<RouteComponentProps<routeInter>>=(props)=>{
-  return (<h1>路由详情页，id:{props.match.params.detailId}</h1>)
-}
+export const DetailPage: React.FC<RouteComponentProps<MatchParams>> = (
+  props
+) => {
+  //useParams 返回URL参数的键/值对的对象
+  const { detailId } = useParams<MatchParams>();
+
+  const [loading, setLoading] = useState<boolean>(true);
+  const [product, setProduct] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get(
+          "http://123.56.149.216:8080/api/productCollections"
+        );
+        setProduct(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <h1>
+      路由详情页，id:{props.match.params.detailId}:{detailId}
+      {/* 渲染html */}
+      {/* <div
+          dangerouslySetInnerHTML={{ __html: product.features }}
+          style={{ margin: 50 }}
+        ></div> */}
+    </h1>
+  );
+};
